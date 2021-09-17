ae02baa4971bf5fc2578654382222823cb4c81c1
sh1540 <pararrelspace@gmail.com>
2021-09-17 23:01:58 +0900

    1. 페이지 내에서 메뉴 선택 시 활성 상태 표현 (NavLink 적용)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,25 @@
 import React from 'react';
-import {Link, Route} from "react-router-dom";
+import {NavLink, Route} from "react-router-dom";
 
 import Home from "./components/Home";
 import Timetable from "./components/Timetable";
 import Setting from "./components/Setting";
 
 
+const activeStyle = {
+    background: 'black',
+    color: 'white',
+};
+
+
 const App = () => {
+
     return (
         <div>
           <ul>
-            <li><Link to={"/"}>홈</Link></li>
-            <li><Link to={"/timetable"} exact={true}>시간표</Link></li>
-            <li><Link to={"/setting"}>설정</Link></li>
+            <li><NavLink to={"/"} activeStyle={activeStyle} exact>홈</NavLink></li>
+            <li><NavLink to={"/timetable"}  activeStyle={activeStyle}>시간표</NavLink></li>
+            <li><NavLink to={"/setting"} activeStyle={activeStyle}>설정</NavLink></li>
           </ul>
           <hr />
           <div>
